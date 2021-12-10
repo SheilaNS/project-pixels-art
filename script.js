e7182a5f3ce838122dd4f6e9d7d4118ec5d233e1
SheilaNS <shei.nsantos@gmail.com>
2021-12-10 02:07:34 -0300

    Requisito 9 ok

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const black = document.getElementById('black');
 const red = document.getElementById('red');
 const yellow = document.getElementById('yellow');
 const blue = document.getElementById('blue');
+const button = document.getElementById('clear-board');
 
 function addClass(event) {
   const withClass = document.querySelector('.selected');
@@ -23,3 +24,12 @@ for (let i = 0; i < 25; i += 1) {
   const pixel = document.getElementsByClassName('pixel');
   pixel[i].addEventListener('click', changeColor);
 }
+
+function cleanBoard() {
+  for (let i = 0; i < 25; i += 1) {
+    const clear = document.querySelectorAll('.pixel');
+    clear[i].style.backgroundColor = 'white';
+  }
+}
+
+button.addEventListener('click', cleanBoard);
